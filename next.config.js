0778f303da569d718c769ba2e3c1405a70d0595b
Pedro <pedro.spinsomewebs@gmail.com>
2022-05-02 12:35:43 -0400

    Add bundle optimizer plugin

--- next.config.js
+++ next.config.js
@@ -3,4 +3,9 @@ const nextConfig = {
   reactStrictMode: true,
 }
 
-module.exports = nextConfig
+const withPlugins = require('next-compose-plugins')
+const withBundleAnalyzer = require('@next/bundle-analyzer')({
+  enabled: process.env.ANALYZE === 'true',
+})
+
+module.exports = withPlugins([[withBundleAnalyzer]], nextConfig)
